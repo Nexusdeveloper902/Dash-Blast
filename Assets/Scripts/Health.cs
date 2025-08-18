d71975eb1f8d72b38b52bb881a7548d5baf97517
Nexusdeveloper902 <jeroperez2011@gmail.com>
2025-08-18 12:47:06 -0500

    Minor refactoring and bug fixing

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,23 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Health : MonoBehaviour
 {
     public float maxHealth;
     public float currentHealth;
+    [SerializeField] private Color colorFlash;
+    
+    private SpriteRenderer spriteRenderer;
+    private Color previousColor;
 
     IEnumerator Start()
     {
         yield return new WaitForEndOfFrame();
         currentHealth = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+        previousColor = spriteRenderer.color;
+        colorFlash.a = 255;
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth  -= damage;
+        StartCoroutine(Flash());
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator Flash()
+    {
+        spriteRenderer.color = colorFlash;
+        yield return new WaitForSeconds(0.15f);
+        spriteRenderer.color = previousColor;
     }
 }
